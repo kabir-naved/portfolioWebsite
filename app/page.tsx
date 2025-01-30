5505124f1bb5be6a784e4509c448bf46f3f2a619
code <kabir.naved.khan@gmail.com>
2025-01-30 09:51:29 +0530

    hero section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[{ name: "Home", link: "/" }, { name: "About", link: "/Portfolio" }]} />
         <Hero />
       </div>
     </main>
