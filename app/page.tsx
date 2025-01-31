1ef2fbe72528c07186221b2239aa88849f56c3c1
code <kabir.naved.khan@gmail.com>
2025-01-31 21:43:55 +0530

    navbar is completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{ name: "Home", link: "/" }, { name: "About", link: "/Portfolio" }]} />
+        <FloatingNav navItems={[{ name: "Home", link: "/", icon: <FaHome/> }, { name: "About", link: "/Portfolio" }]} />
         <Hero />
       </div>
     </main>
